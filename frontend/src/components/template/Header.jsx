ab9e79891694a7079c1febeb47b8a33441d636bf
alandutra02 <alandutra02@yahoo.com.br>
2025-06-10 16:56:47 -0300

    header

--- frontend/src/components/template/Header.jsx
+++ frontend/src/components/template/Header.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import './Header.css';
 
 /* d-none é para sumir com os cabeçalhos para dispositivos celulares. d-sm-flex é para dispositivos medios para cima. flex-column que define a orientação na vertical, o padrão é flex-row, então definimos */
+/* fa quer dizer font-awesome, que também tem ícones. fa-${props.icon} vai receber o conteudo de props.icon, que é "home". Então ficará "fa-home" o que vai gerar o icone da casinha (home)*/ 
+/* props.title vai receber a palavra início, que foi definida em App.jsx*/
 export default props =>
     <header className="header d-none d-sm-flex flex-column">
         <h1 className="mt-3">
