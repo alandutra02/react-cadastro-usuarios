31fd586628343c37d8f68e5fbd621b533e0fd20e
alandutra02 <alandutra02@yahoo.com.br>
2025-06-10 16:43:25 -0300

    header

--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import './Main.css';
 import Header from "./Header";
 
+// Obs: o componente Header está sendo importado. Ele é usado dentro de props com o nome Header. Quando usamos o Header {...props} estamos importando todas as propriedades que foram definidas no arquivo App.js. Essas propriedades são "icon, "title"e "subtitle"
 export default props =>
     <>
-        <Header />
+        <Header {...props} />
         <main className="content">
             Conteúdo
         </main>
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css'; /* Vamos importar o boostrap e font waesome que estão em node_modules depois de instalar as dependências*/
+import 'font-awesome/css/font-awesome.min.css';
+
 import Logo from '../components/template/Logo';
 import Nav from '../components/template/Nav';
 import Main from "../components/template/Main";
 import Footer from "../components/template/Footer";
 
+// Observe que o componente Main definido dentro de props, também está ligado ao componente Header porque Main.jsx importa Header.jsx. Ou seja, as propriedades (props) que foram recebidas no Main são passadas para Header
 export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main />
+        <Main icon="home" title="inicio" 
+            subtitle="Segundo Projeto - Cadastro de Usuários"/>
         <Footer />
     </div>
